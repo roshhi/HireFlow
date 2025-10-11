165f0d7d1d0f2d34356b9fb672dcc679951cea5d
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-11 13:21:31 +0500

    Added a hero component.

---
+++ src/components/hero.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Hero = ()=>{
+    return (
+        <>
+            <div className="w-full flex-center flex-col h-[20vh] bg-indigo-600">
+                <p className="text-[min(7.5vw,60px)] font-bold">Become a React Dev</p>
+                <p className="text-[min(5vw,18px)]" >Find the React  job that fits yours skill set</p>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
