72ed873f52e55fbb9e04e2248ec241068a54bbc9
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-12 14:38:06 +0500

    Intergrated the spinner logic in the jobListings.jsx file.

--- src/components/jobListings.jsx
+++ src/components/jobListings.jsx
@@ -1,34 +1,65 @@
 import React from "react";
 import Job from "./job";
-import myJobs from '../jobs.json'
 import ViewAllJobs from "./viewAllJobs";
-import { useState } from "react";
+import Spinner from "./spinner";
+import { useState,useEffect } from "react";
 
-const JobListings = ()=>{
+const JobListings = ( {isHome = false} )=>{
 
-    let [visibleCount,setVisibleCount] = useState(3);
-    const limJobs = myJobs.slice(0, visibleCount);
+    const [jobs,setJobs] = useState([]);
+    const [loading,setloading] = useState(true);
+
+    useEffect( ()=>{
+        const fetchJobs = async() => {
+            try {
+                const url = "http://localhost:8000/jobs";
+                const res = await fetch (url)
+                const data = await res.json();
+                console.log(data);
+                setJobs(data);
+            } catch (error) {
+                console.log("Oops Error Fetching Data !!!",error)
+            } finally{
+                setloading(false);
+            }
+        }
+        fetchJobs();
+    },[])
+
+    const initialCount = isHome ? 6 : 9;
+    let [visibleCount,setVisibleCount] = useState(initialCount);
+    const limJobs = jobs.slice(0, visibleCount);
     const loadMore = ()=>{
         setVisibleCount((prev)=>prev+3);
     }
     return (
         <>
-            <p className=" flex-center text-[rgb(57,55,241)] bg-[rgb(236,244,255)]  font-bold text-[min(6.8vw,35px)] pt-3 pb-3">Browse Jobs</p>
-            <div className=" grid grid-cols-1 gap-3 place-items-center md:grid-cols-2 xl:grid-cols-3 bg-[rgb(236,244,255)] w-full h-auto pt-3">
-                {   limJobs.map((job) => { 
-                    return (
-                        <Job
-                          key={job.id}               
-                          type={job.type}
-                          title={job.title}
-                          desc={job.description}
-                          sal={job.salary}
-                          loc={job.location}
-                        />
-                    )
-                })}
-            </div>
-            <ViewAllJobs onClick = {loadMore}/>
+            <p className=" flex-center text-[rgb(57,55,241)] bg-[rgb(236,244,255)]  font-bold text-[min(6.8vw,35px)] pt-[25px] pb-[45px]">
+                {isHome?'Recent Jobs':'Browse Jobs'}
+            </p>
+
+            {loading ? <Spinner loading={loading}/> :
+            <>
+                <div className=" grid grid-cols-1 gap-3 place-items-center md:grid-cols-2 xl:grid-cols-3 bg-[rgb(236,244,255)] w-full h-auto pt-3">
+                   {limJobs.map((job) => { 
+                        return (
+                            <Job
+                            key={job.id}               
+                            type={job.type}
+                            title={job.title}
+                            desc={job.description}
+                            sal={job.salary}
+                            loc={job.location}
+                            />
+                        )
+                    })}
+                </div>
+
+                <ViewAllJobs onClick = {loadMore}/>
+            </>    
+            }
+            
+
         </> 
     )
 }
