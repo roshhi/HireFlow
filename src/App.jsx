8be8512b7972fb88a07538eefe4ebb968e4cb4e4
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-12 14:34:32 +0500

    Added a notFoundPage in the app.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { Route,createBrowserRouter,createRoutesFromElements,RouterProvider } from 'react-router-dom'
 import React from 'react'
-import HomePage from '../pages/homePage'
 import MainLayout from './layouts/MainLayout'
+import HomePage from '../pages/homePage'
+import JobsPage from '../pages/jobsPage'
+import NotFoundPage from '../pages/notFoundPage'
 
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element = {<MainLayout/>}>
     <Route index element={<HomePage/>}/>
+    <Route path='/jobs' element={<JobsPage/>}/>
+    <Route path='*' element={<NotFoundPage/>}/>
   </Route> 
 ));
 
