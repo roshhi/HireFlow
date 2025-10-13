9d144917d5ee67720fcfc6f46e7d81aca547f7ea
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-13 16:36:06 +0500

    Added the route of the "AddJobsPage" in the app.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,23 @@ import MainLayout from './layouts/MainLayout'
 import HomePage from '../pages/homePage'
 import JobsPage from '../pages/jobsPage'
 import NotFoundPage from '../pages/notFoundPage'
+import JobPage from '../pages/JobPage'
+import AddJobsPage from '../pages/AddJobsPage'
 
+const jobLoader = async({ params })=>{
+  const url = `/api/jobs/${params.id}`;
+  const res = await fetch (url)
+  const data = await res.json();
+  return data;
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
   <Route path='/' element = {<MainLayout/>}>
     <Route index element={<HomePage/>}/>
     <Route path='/jobs' element={<JobsPage/>}/>
+    <Route path='/jobs/:id' element={<JobPage/>} loader = {jobLoader}/>
+    <Route path='/addjob' element={<AddJobsPage/>}/>
     <Route path='*' element={<NotFoundPage/>}/>
   </Route> 
 ));
