286f8e60d4c952733848ba65f3c732f1af6efad5
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-15 18:18:59 +0500

    Corrected the typo in the import statement from navbar to Navbar as i figured it out while hosting my project on vercel.

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Navbar from '../components/navbar'
+import Navbar from '../components/Navbar'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/ReactToastify.css'
 
