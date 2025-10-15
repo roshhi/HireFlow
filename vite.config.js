8bcdfea4ecfa979ccb2a5f7084d0ddc3cfe384e0
MUHAMMAD-AHMAD-07 <not.roshaan@gmail.com>
2025-10-15 19:55:54 +0500

    updated the vite config file to allow hireflow-5.onrender.com

--- vite.config.js
+++ vite.config.js
@@ -16,5 +16,8 @@ export default defineConfig({
           rewrite: (path) => path.replace(/^\/api/, ''),
         },
       },
+      preview: {
+        allowedHosts: ['hireflow-5.onrender.com']
+      }
   },
 })
